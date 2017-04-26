8b8d0c3882d9719f9339713c478ae1405d6330c7
Chris.D <fusion101box@gmail.com>
2017-04-26 11:55:28 +0200

    Adding each activity to create the structure

---
+++ app/src/main/java/fr/sharpbunny/emergencytag/AddElement.java
@@ -0,0 +1,14 @@
+package fr.sharpbunny.emergencytag;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddElement extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_element);
+    }
+}
