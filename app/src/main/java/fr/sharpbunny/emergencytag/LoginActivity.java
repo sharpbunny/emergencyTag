b0b8129e8b8a9a9bde02e0a0f050b680f09cb736
Juliengng <gagniare.j@gmail.com>
2017-04-26 16:25:02 +0200

    accès Info activity Ok login:info mpd:info

--- app/src/main/java/fr/sharpbunny/emergencytag/LoginActivity.java
+++ app/src/main/java/fr/sharpbunny/emergencytag/LoginActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,20 +17,25 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-    public void Login(View v)
-    {
-        {
-            EditText ChampLogin = (EditText) findViewById(R.id.ChampLogin);
-            EditText ChampPassword = (EditText) findViewById(R.id.ChampPassword);
-            if (ChampLogin.getText().toString().equals("1234") && ChampPassword.getText().toString().equals("1234")) {
-                Toast.makeText(this, "Welcome here !", Toast.LENGTH_SHORT).show();//correct password
-                Intent intent = new Intent(this, InfoList.class);
-                startActivity(intent);
-            } else {
-                Toast.makeText(this, "You shall not pass !", Toast.LENGTH_SHORT).show();//incorrect password
+        Button buttonconnexion = (Button) findViewById(R.id.buttonLogin);
+        buttonconnexion.setOnClickListener
+                (
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EditText ChampLogin = (EditText) findViewById(R.id.ChampLogin);
+                        EditText ChampPassword = (EditText) findViewById(R.id.ChampPassword);
+                        if (ChampLogin.getText().toString().equals("info") && ChampPassword.getText().toString().equals("info")) {
+                            Toast.makeText(LoginActivity.this, "Welcome here !", Toast.LENGTH_SHORT).show();//correct password
+                            Intent intent = new Intent(LoginActivity.this, InfoList.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "You shall not pass !", Toast.LENGTH_SHORT).show();//incorrect password
+                        }
+                    }
 
-            }
-        }
+                    }
+                );
     }
-}
+    }
+
