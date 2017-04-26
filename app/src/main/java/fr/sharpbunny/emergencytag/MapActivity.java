759f6fbee6eb67d3a200833a9541e2212419515b
FRANCE Frédéric <frederic.france@free.fr>
2017-04-26 14:52:49 +0200

    correct merge

--- app/src/main/java/fr/sharpbunny/emergencytag/MapActivity.java
+++ app/src/main/java/fr/sharpbunny/emergencytag/MapActivity.java
@@ -1,14 +1,20 @@
 package fr.sharpbunny.emergencytag;
 
-import android.app.Activity;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MapActivity extends Activity {
+public class EmergMapActivity extends MapActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+    }
+    @Override
+    protected boolean isRouteDisplayed() {
+        return false;
     }
 }
