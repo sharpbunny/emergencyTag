875f34953ed7ce6314ac0166aa398c6232da44a1
Chris.D <fusion101box@gmail.com>
2017-04-26 14:27:32 +0200

    Lock the positionning and Add a random namelist

--- app/src/main/java/fr/sharpbunny/emergencytag/InfoList.java
+++ app/src/main/java/fr/sharpbunny/emergencytag/InfoList.java
@@ -3,12 +3,28 @@ package fr.sharpbunny.emergencytag;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class InfoList extends Activity {
+
+    protected ListView displayList;
+    String[] prenoms = new String[]{
+            "Antoine", "Benoit", "Cyril", "David", "Eloise", "Florent",
+            "Gerard", "Hugo", "Ingrid", "Jonathan", "Kevin", "Logan",
+            "Mathieu", "Noemie", "Olivia", "Philippe", "Quentin", "Romain",
+            "Sophie", "Tristan", "Vincent", "Willy", "Xavier",
+            "Yann", "Zoé"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_list);
+
+        displayList = (ListView)findViewById(R.id.listElement);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(InfoList.this,
+                android.R.layout.simple_list_item_1, prenoms);
+        displayList.setAdapter(adapter);
     }
 }
