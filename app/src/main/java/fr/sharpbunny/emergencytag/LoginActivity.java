cfbae10f6c195d52e53d798be41b9ce1e222ae09
Juliengng <gagniare.j@gmail.com>
2017-04-26 14:54:27 +0200

    Maj activity Login

--- app/src/main/java/fr/sharpbunny/emergencytag/LoginActivity.java
+++ app/src/main/java/fr/sharpbunny/emergencytag/LoginActivity.java
@@ -3,6 +3,12 @@ package fr.sharpbunny.emergencytag;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
@@ -10,5 +16,20 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void Login(View v)
+    {
+        {
+            EditText login = (EditText) findViewById(R.id.ChampLogin);
+            EditText password = (EditText) findViewById(R.id.ChampPassword);
+            if (login.getText().toString().equals("1234") && password.getText().toString().equals("1234")) {
+                Toast.makeText(this, "Welcome here !", Toast.LENGTH_SHORT).show();//correct password
+                Intent intent = new Intent(this, InfoList.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "You shall not pass !", Toast.LENGTH_SHORT).show();//incorrect password
+
+            }
+        }
     }
 }
