929c94106e9950b685aa45f9b3e39237e34103e9
jringenbach <34011-14-08@exchange.ad.afpanet>
2017-04-27 09:44:24 +0200

    Regroupement de l'initialisation des éléments de l'activité dans une méthode initialiseComponents

--- app/src/main/java/fr/sharpbunny/emergencytag/AddElement.java
+++ app/src/main/java/fr/sharpbunny/emergencytag/AddElement.java
@@ -12,18 +12,27 @@ import android.widget.Spinner;
  * Permet d'ajouter un item à la base de données en inscrivant son type, sa photo et sa description
  */
 public class AddElement extends Activity {
-    Spinner typeElementSpinner;
-    Button boutonValider = (Button)findViewById(R.id.validerPhotoButton);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        insertionElementSpinner(typeElementSpinner);
-        boutonValider.setOnClickListener(clickListenerValider);
+        initialiseComponents();
 
 
         setContentView(R.layout.activity_add_element);
+    }
+
+    /**
+     * Initialise tous les éléments de la page
+     */
+    private void initialiseComponents(){
+        Spinner typeElementSpinner = (Spinner)findViewById(R.id.typeSpinner);
+        Button boutonValider = (Button)findViewById(R.id.validerPhotoButton);
+
+        insertionElementSpinner(typeElementSpinner);
+        boutonValider.setOnClickListener(clickListenerValider);
     }
 
     /**
@@ -31,8 +40,6 @@ public class AddElement extends Activity {
      * @param elementSpinner
      */
     private void insertionElementSpinner(Spinner elementSpinner){
-        elementSpinner = (Spinner)findViewById(R.id.typeSpinner);
-
         //ArrayAdapter: Tableau contenant un item par case
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.typeItemArray, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
