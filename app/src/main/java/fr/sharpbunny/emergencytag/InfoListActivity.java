79675e84b2f372fc63b184aca600cae89ac824a1
FRANCE Frédéric <frederic.france@free.fr>
2017-05-08 22:52:35 +0200

    add menu in info list

--- app/src/main/java/fr/sharpbunny/emergencytag/InfoListActivity.java
+++ app/src/main/java/fr/sharpbunny/emergencytag/InfoListActivity.java
@@ -5,8 +5,12 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import android.widget.AdapterView;
@@ -29,18 +33,18 @@ public class InfoListActivity extends AppCompatActivity {
     private ListView mListView;
     private JSONObject jsonResp;
 
-    Button addNewElementBtn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         Log.d(TAG, "Entering infolist...");
+
         setContentView(R.layout.activity_info_list);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        addNewElementBtn = (Button)findViewById(R.id.accesNewElement);
-
-
+        FloatingActionButton addNewElementBtn = (FloatingActionButton)findViewById(R.id.accesNewElement);
 
         addNewElementBtn.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -54,6 +58,31 @@ public class InfoListActivity extends AppCompatActivity {
 
         // Calling async task to get item list
         new GetJsonItem().execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_info_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_map) {
+            // go to map activity
+            Intent intent = new Intent(InfoListActivity.this,MapActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     /**
